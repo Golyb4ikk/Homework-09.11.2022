9c813dff9c2e638231d066c17a4796084895d950
golyb4ik <only_game2@mail.ru>
2022-11-21 17:47:35 +0300

    1.4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-       int lP=23;
-       int aS=27;
-       int eA=30;
-       int allPaper=480;
-       int everyOne;
-       everyOne= 480/(lP+aS+eA);
-        System.out.println("На каждого ученика расчитано "+everyOne+" листов бумаги");
+       int bottles=16;
+       int time=2;
+       int proizv=16/2;
+       int proizv1=proizv*60;
+       int result;
+       result=proizv*20;
+        System.out.println("За 20 мин машина произвела бутылок "+result+" штук");
+        int result1=proizv1*24;
+        System.out.println("За 20 мин машина произвела бутылок "+result1+" штук");
+        int result2=proizv1*24*3;
+        System.out.println("За 20 мин машина произвела бутылок "+result2+" штук");
+        int result3=result1*30;
+        System.out.println("За 20 мин машина произвела бутылок "+result3+" штук");
+
 
 
     }
