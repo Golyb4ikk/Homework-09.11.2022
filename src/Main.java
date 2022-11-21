35b66207598a3929a5bf90ee1a3fde8629d73126
golyb4ik <only_game2@mail.ru>
2022-11-21 17:31:05 +0300

    1.3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        byte a = 67;
-        short c = 569;
-        short v = -159;
-        int y=27897;
-        long d = 987678965549L;
-        double u = 27.12;
-        double g = 2.786;
-        boolean l= false;
+       int lP=23;
+       int aS=27;
+       int eA=30;
+       int allPaper=480;
+       int everyOne;
+       everyOne= 480/(lP+aS+eA);
+        System.out.println("На каждого ученика расчитано "+everyOne+" листов бумаги");
+
+
     }
 }
