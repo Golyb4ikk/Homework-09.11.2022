33013887635c3b04c0901b68d7be1e6441b3a892
golyb4ik <only_game2@mail.ru>
2022-11-21 15:47:10 +0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a=1;
+        byte b=2;
+        short c=3;
+        long d=4888888888888L;
+        float h=5.0f;
+        double u=6.5675;
+        System.out.println("Значение переменной a c типом int равно "+ a);
+        System.out.println("Значение переменной b c типом byte равно "+ b);
+        System.out.println("Значение переменной c c типом short равно "+ c);
+        System.out.println("Значение переменной d c типом long равно "+ d);
+        System.out.println("Значение переменной h c типом float равно "+ h);
+        System.out.println("Значение переменной u c типом double равно "+ u);
     }
+
 }
