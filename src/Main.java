445730bed21703e9ea1b932801db2d8c9cb8602f
golyb4ik <only_game2@mail.ru>
2022-11-22 17:16:28 +0300

    1.5

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-       int bottles=16;
-       int time=2;
-       int proizv=16/2;
-       int proizv1=proizv*60;
-       int result;
-       result=proizv*20;
-        System.out.println("За 20 мин машина произвела бутылок "+result+" штук");
-        int result1=proizv1*24;
-        System.out.println("За 20 мин машина произвела бутылок "+result1+" штук");
-        int result2=proizv1*24*3;
-        System.out.println("За 20 мин машина произвела бутылок "+result2+" штук");
-        int result3=result1*30;
-        System.out.println("За 20 мин машина произвела бутылок "+result3+" штук");
-
+       int whitePaint;
+       int brownPaint;
+       int allPaint=120;
+       int Class=120/(2+4);
+       whitePaint=120/(2+4)*2;
+       brownPaint=120/(2+4)*4;
+        System.out.println("В школе,где "+Class+" классов,нужно "+whitePaint+" банок белой краски и "+brownPaint+" банок коричневой краски");
 
 
     }
